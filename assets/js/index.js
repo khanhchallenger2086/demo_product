92393226f4f66a45bea0786574c829eb422805e2
lam khanh <khanhchallenger2086@gmail.com>
2024-04-03 01:12:39 +0700

    complete test

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,7 +5,30 @@ $(document).ready(function(){
       dots:true,
       items:3,
       nav:true,
-      autoplay:true
-    
+      autoplay:true,
+      responsive:{
+        0:{
+          items:1,
+          nav:true
+      },
+      600:{
+          items:3,
+      },
+     
+       
+    }
   })
+
+
+  $(".dropdown").click(function(){
+    $(this).find(".dropdown-menu").toggleClass('menu-li-in');
+  });
+
+  $(".menu-reponsive-button p").click(function(){
+    $(".menu-reponsive-xs").toggleClass('menu-in');
+  });
+
+  $(".close-menu").click(function(){
+    $(".menu-reponsive-xs").toggleClass('menu-in');
+  });
 });
